f8037271299325c438aaaf569f8be00e716b467f
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-05 12:23:49 +0000

    [Add] Userモデルのidをstring型に変更
    Userモデルのidをランダム生成したidにするため、string型に変更。

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_05_120913) do
+
+  create_table "users", id: :string, force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "email", default: "", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "account_name", null: false
+    t.string "profile_image_id", null: false
+    t.string "introduction", null: false
+    t.boolean "is_active", default: true, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
