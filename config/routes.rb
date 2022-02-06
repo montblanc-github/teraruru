459103d452faf0f66dbf57b63b0e9a27acf80c40
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-06 08:25:53 +0000

    [Fix] User側ルーティング修正
    deviseによって自動生成されたルーティングだと、ファイルパスが異なるため、修正。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  # ユーザ側ルーティング
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
