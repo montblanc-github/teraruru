7d6cd45a4af0be8eb9dad2fb6342e889c7b86897
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-06 09:55:50 +0000

    [Add] User側ルーティングを追加
    deviseでは自動生成されない、マイページなどのルーティングを追加したため。

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:edit, :show]
+
 end
