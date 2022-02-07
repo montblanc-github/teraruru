f49435b1a51ab06e2a9b0f8ed4a424479e0b2f95
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-07 13:52:04 +0000

    [Add] ユーザ情報編集機能＆編集画面作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,7 +4,25 @@ class Public::UsersController < ApplicationController
 
   end
 
-  def mypage
-
+  def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザ情報を変更しました。"
+      redirect_to user_path
+    else
+      flash[:notice] = "ユーザ情報の変更に失敗しました。"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :account_name, :profile_image, :introduction)
+  end
+
 end
