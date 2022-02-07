0afac7c6f3219ce55940a6cd7fde87d401de3afc
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-07 13:52:04 +0000

    [Add] ユーザ情報編集機能＆編集画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
+    get "users/index"
+    get "users/show"
+    get "users/edit"
   end
   # ユーザ側ルーティング
-  devise_for :users, controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
+  devise_for :users, skip: [:registrations], controllers: {
+    sessions: "public/sessions",
+    passwords: "public/passwords"
   }
 
+  devise_scope :user do
+    get 'users/cancel', to: 'public/registrations#cancel', as: :cancel_user_registration
+    get 'users/sign_up', to: 'public/registrations#new', as: :new_user_registration
+    resource :users, only: [:create], as: 'user_registration', controller: 'public/registrations'
+  end
+
   scope module: :public do
-    resources :users, only: [:edit, :show]
+    resources :users, only: [:show, :edit, :update]
 
     # homes
     root "homes#top"
