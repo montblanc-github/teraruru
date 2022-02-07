acf32463774308c39073e1899d5f1f6db2c5af34
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-07 14:50:35 +0000

    [Add] Articleコントローラーの生成

---
+++ app/controllers/public/articles_controller.rb
@@ -0,0 +1,13 @@
+class Public::ArticlesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/public/articles_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class Public::ArticlesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get public_articles_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get public_articles_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get public_articles_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get public_articles_edit_url
+    assert_response :success
+  end
+
+end
