10ccdee2ab71183994b853150dab4ef4579a4d71
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-07 08:38:05 +0000

    [Add] Admin側ログアウト機能

--- db/migrate/20220207075021_devise_create_admins.rb
+++ db/migrate/20220207075021_devise_create_admins.rb
@@ -2,7 +2,7 @@
 
 class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
   def change
-    create_table :admins do |t|
+    create_table :admins, id: :string do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
