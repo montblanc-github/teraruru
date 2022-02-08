c5283583b50d45a9750f3252fdede942b882d095
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-08 02:44:50 +0000

    [Add] 投稿新規作成画面の一時保存
    都道府県と市区町村のデータを作成するため、一時保存

--- app/controllers/public/articles_controller.rb
+++ app/controllers/public/articles_controller.rb
@@ -6,8 +6,26 @@ class Public::ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.create
+      flash[:notice] = "投稿を作成しました。"
+      redirect_to article_path
+    else
+      flash[:notice] = "投稿の作成に失敗しました。"
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:artical_image_id, :cultivar_name, :prefecture, :municipality, :level, :season, :fertilizer_existence, :fertilizer_info, :place, :condition, :state_at_start, :message)
   end
 end
