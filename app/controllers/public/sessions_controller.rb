3d722c3e1dcf17bc062a3dfecd7ba292fba62010
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-12 11:13:41 +0000

    [Add] User利用停止機能を追加
    管理者側からUserの利用停止ができるよう機能を追加したため。

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +18,20 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.valid_password?(params[:user][:password]) && @user.is_active == false
+      flash[:notice] = "このアカウントは、利用停止中です。詳細は、Aboutページをご覧ください。"
+      redirect_to root_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
