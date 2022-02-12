b2cd06047a4f0ef486e1928f418738e130729343
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-12 11:43:45 +0000

    [Add] フォロー機能コントローラー作成

---
+++ app/controllers/public/relationships_controller.rb
@@ -0,0 +1,24 @@
+class Public::RelationshipsController < ApplicationController
+before_action :authenticate_user!
+
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
+
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+
+end
