f430d9ce7dfacb308443c65753997834b88a4ddb
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-13 14:16:50 +0000

    [Add] DM機能メッセージ表示画面の作成

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -12,13 +12,13 @@ before_action :authenticate_user!, except: [:followings, :followers]
   end
 
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers
   end
 
 end
