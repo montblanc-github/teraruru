26b1a8c059455427c8a18dd3410f3242e8d2683d
mont-blanc <86041660+montblanc-github@users.noreply.github.com>
2022-02-14 03:02:50 +0900

    [Add] コメント機能追加

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,8 +2,8 @@ class Public::CommentsController < ApplicationController
     before_action :authenticate_user!
 
     def create
-        article = Article.find(params[:id])
-        comment = current_user.comments.new(comment_param)
+        article = Article.find(params[:article_id])
+        comment = current_user.comments.new(comment_params)
         comment.article_id = article.id
         comment.save
         redirect_to article_path(article)
