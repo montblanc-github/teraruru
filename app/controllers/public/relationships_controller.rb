2bf2e0c4465d836abf97364d6b4bbaedd5d6d271
mont-blanc <86041660+montblanc-github@users.noreply.github.com>
2022-02-14 21:49:34 +0900

    [Add] フォロー機能の非同期通信化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,13 +2,13 @@ class Public::RelationshipsController < ApplicationController
 before_action :authenticate_user!, except: [:followings, :followers]
 
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
   end
 
   def followings
