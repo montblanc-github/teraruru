bbe64ffc1bb1b7598c163d7b773cd23c6f768870
mont-blanc <86041660+montblanc-github@users.noreply.github.com>
2022-02-14 11:06:15 +0900

    [Add] フォローしているユーザのみの投稿タイムライン作成
    フォローしているユーザのみの投稿を表示するタイムラインをtopページに追加したため。

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
     @articles = Article.page(params[:page]).per(6)
+    user = User.find(current_user.id)
+    following_users = user.followings
+    @following_articles = Article.where(user_id: following_users).order(created_at: :desc).page(params[:top_article_page]).per(6)
   end
 
   def about
