a94b2d9e214a0bd3db26860c854925fac7b0b7a8
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-16 09:55:53 +0000

    [Fix] 新規登録後ルートエラー修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -52,7 +52,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    user_path(current_user)
+    user_path(resource)
   end
 
   # The path used after sign up for inactive accounts.
