f35e61fd59e0aa4506647e364f9a93aedd6e205c
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-16 13:27:56 +0000

    [Add] ゲストログイン機能のメソッドをモデルに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,6 +79,14 @@ class User < ApplicationRecord
     end
   end
 
+  # ゲストログイン機能
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
   private
 
   # idをuuidに変更。
