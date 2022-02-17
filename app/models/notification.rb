df256788cc6c01c6c63de2e38556a660ab7ff467
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-17 02:43:36 +0000

    [Add] 通知機能&フォロー機能へのバリデーション追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,4 +6,8 @@ class Notification < ApplicationRecord
     belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
     belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
 
+    # バリデーション
+    validates :visited_id, presence: true
+    validates :action, presence: true
+
 end
