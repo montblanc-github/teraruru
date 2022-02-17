7ba94c2d9794a688a17a64adb4597bc85f356cfc
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-17 06:49:06 +0000

    [Add] devise関連エラーメッセージの日本語化

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Teraruru
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
