101d57dfa17cd4740e4fb102fecfa8cd3fc70578
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-17 09:17:11 +0000

    [Fix] ユーザ情報のバリデーション修正
    正規表現が意図しない動きをしたため、正しい内容になるよう修正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,8 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   # 正規表現
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]\z/
-  VALID_KANA_REGEX = /\A[ァ-ヴー－]\z/
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_KANA_REGEX = /\A[ァ-ヴー－]+\z/
   VALID_EMAIL_REGEX = /\A\S+@\S+\.\S+\z/
   VALID_POST_CODE_REGEX = /\A\d{7}\z/
 
