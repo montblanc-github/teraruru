bb744ec9509c523c913a77079ca890a86617f690
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-17 05:49:09 +0000

    [Add] エラーメッセージ日本語化gem追加

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Teraruru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
