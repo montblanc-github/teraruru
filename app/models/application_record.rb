807afd03f24b53ab1fba449bd569083f34c54256
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-17 02:28:45 +0000

    [Add] 正規表現&管理者バリデーション追加
    正規表現の表記を追加し、管理者バリデーションを追加したため。

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # 正規表現
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]\z/
+  VALID_KANA_REGEX = /\A[ァ-ヴー－]\z/
+  VALID_EMAIL_REGEX = /\A\S+@\S+\.\S+\z/
+  VALID_POST_REGEX = /\A\d{7}\z/
+
 end
