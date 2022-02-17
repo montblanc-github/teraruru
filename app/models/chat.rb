5c2650485298d679a99401624332b4c81b71b719
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-17 02:39:54 +0000

    [Add] DM&コメント機能へのバリデーション追加

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,4 +2,8 @@ class Chat < ApplicationRecord
   # アソシエーション
   belongs_to :room
   belongs_to :user
+
+  # バリデーション追加
+  validates :content, presence: true
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
   # アソシエーション
   belongs_to :user
   belongs_to :article
+
+  # バリデーション
+  validates :content, presence: true
+
 end
