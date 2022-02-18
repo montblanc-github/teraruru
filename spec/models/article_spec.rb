a031e6a231ff175c8033f9700a44a5a3f04d3a2b
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-18 12:38:38 +0000

    [Add] 投稿バリデーションテスト追加

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Articleモデルのテスト', type: :model do
     subject { article.valid? }
 
     let(:user) { create(:user) }
-    let(:article) { build(:article, user_id: user.id) }
+    let!(:article) { build(:article, user_id: user.id) }
 
     context 'cultivar_nameカラム' do
       it '空欄でないこと' do
@@ -16,6 +16,11 @@ RSpec.describe 'Articleモデルのテスト', type: :model do
         article.cultivar_name = Faker::Lorem.characters(number: 21)
         is_expected.to eq true
       end
+      it '21文字以下であること: 22文字は×' do
+        article.cultivar_name = Faker::Lorem.characters(number: 22)
+        is_expected.to eq false
+      end
+
     end
 
     context 'levelカラム'do
@@ -39,6 +44,36 @@ RSpec.describe 'Articleモデルのテスト', type: :model do
       end
     end
 
+    context 'fertilizer_existenceカラム'do
+      it '空欄でないこと' do
+        article.fertilizer_existence = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'fertilizer_infoカラム'do
+      it 'fertilizer_existenceがtrueの時、空欄でないこと' do
+        article.fertilizer_existence = 'true'
+        article.fertilizer_info = ''
+        is_expected.to eq false
+      end
+      it 'fertilizer_existenceがfalseの時、空欄でも〇' do
+        article.fertilizer_existence = 'false'
+        article.fertilizer_info = ''
+        is_expected.to eq true
+      end
+      it '100文字以下であること: 100文字は〇' do
+        article.fertilizer_existence = 'true'
+        article.fertilizer_info = Faker::Lorem.characters(number: 100)
+        is_expected.to eq true
+      end
+      it '100文字以下であること: 101文字は×' do
+        article.fertilizer_existence = 'true'
+        article.fertilizer_info = Faker::Lorem.characters(number: 101)
+        is_expected.to eq false
+      end
+    end
+
     context 'placeカラム'do
       it '空欄でないこと' do
         article.place = ''
@@ -46,8 +81,38 @@ RSpec.describe 'Articleモデルのテスト', type: :model do
       end
     end
 
+    context 'conditionカラム'do
+      it '空欄でないこと' do
+        article.condition = ''
+        is_expected.to eq false
+      end
+    end
 
+    context 'state_at_startカラム'do
+      it '空欄でないこと' do
+        article.state_at_start = ''
+        is_expected.to eq false
+      end
+    end
 
+    context 'tag_listカラム'do
+      it '10個以下であること: 10個は〇' do
+        article.tag_list = Faker::Lorem.words(number: 10)
+        is_expected.to eq true
+      end
+      it '10個以下であること: 11個は×' do
+        article.tag_list = Faker::Lorem.words(number: 11)
+        is_expected.to eq false
+      end
+      it '10文字以下であること: 10文字は〇' do
+        article.tag_list = Faker::Lorem.characters(number: 10)
+        is_expected.to eq true
+      end
+      it '10文字以下であること: 11文字は×' do
+        article.tag_list = Faker::Lorem.characters(number: 11)
+        is_expected.to eq false
+      end
+    end
   end
 
   describe 'アソシエーションのテスト' do
