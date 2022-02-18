fa1c960d17bcd281e4753e0f1e92a8d9de7b308e
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-18 03:41:04 +0000

    [Add] 管理者から投稿を一括変更・削除できる機能追加

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -7,15 +7,22 @@ class Admin::ArticlesController < ApplicationController
   end
 
   def update
-    article = Article.find(params[:id])
-    if article.update(article_params)
-      article.create_notification_hidden!
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      @article.create_notification_hidden!
       flash[:notice] = "投稿の表示状態を変更しました。"
-      redirect_to admin_article_path(article.id)
+      redirect_to admin_article_path(@article.id)
     else
       flash[:notice] = "投稿の表示状態変更に失敗しました。"
       render :show
     end
+  end
+
+  def visible_update_all
+    user = User.find(params[:format])
+    user.articles.update_all(is_visible: true)
+    flash[:notice] = "現在表示されているユーザの投稿をすべて表示状態に変更しました。"
+    redirect_to admin_user_path(user.id)
   end
 
   def destroy
@@ -28,6 +35,13 @@ class Admin::ArticlesController < ApplicationController
       flash[:notice] = "投稿の削除に失敗しました。"
       redirect_to admin_article_path(article.id)
     end
+  end
+
+  def destroy_all
+    user = User.find(params[:format])
+    user.articles.destroy_all
+    flash[:notice] = "現在表示されているユーザの投稿をすべて削除しました。"
+    redirect_to admin_user_path(user.id)
   end
 
   private
