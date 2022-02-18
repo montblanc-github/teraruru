96a72616dccc3d65b78d9fedd743ffdc2fd70215
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-18 16:28:23 +0000

    [Add] いいね機能モデルテスト追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,8 @@ class Favorite < ApplicationRecord
   # アソシエーション
   belongs_to :user
   belongs_to :article
+
+  # バリデーション
+  validates :user_id, presence: true
+  validates :article_id, presence: true
 end
