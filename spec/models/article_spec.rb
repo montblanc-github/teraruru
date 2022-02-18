cab29d7ac3d8e46f0efe96a1b1768dd1ce0cd891
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-18 11:40:29 +0000

    [Fix] 投稿と都道府県/市区町村とのアソシエーション削除
    投稿と都道府県/市区町村の間に不要なアソシエーションがあったため、削除。

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,14 +5,48 @@ RSpec.describe 'Articleモデルのテスト', type: :model do
     subject { article.valid? }
 
     let(:user) { create(:user) }
-    let!(:article) { build(:article, user_id: user.id) }
+    let(:article) { build(:article, user_id: user.id) }
 
     context 'cultivar_nameカラム' do
       it '空欄でないこと' do
         article.cultivar_name = ''
         is_expected.to eq false
       end
+      it '21文字以下であること: 21文字は〇' do
+        article.cultivar_name = Faker::Lorem.characters(number: 21)
+        is_expected.to eq true
+      end
     end
+
+    context 'levelカラム'do
+      it '空欄でないこと' do
+        article.level = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'categoryカラム'do
+      it '空欄でないこと' do
+        article.category = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'seasonカラム'do
+      it '空欄でないこと' do
+        article.season = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'placeカラム'do
+      it '空欄でないこと' do
+        article.place = ''
+        is_expected.to eq false
+      end
+    end
+
+
 
   end
 
