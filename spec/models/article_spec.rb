c6f6f6b25e4497be36622cfe19303c3d1d716265
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-18 04:51:44 +0000

    [Add] 投稿の品種名バリデーションテスト追加

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Articleモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { article.valid? }
+
+    let(:user) { create(:user) }
+    let!(:article) { build(:article, user_id: user.id) }
+
+    context 'cultivar_nameカラム' do
+      it '空欄でないこと' do
+        article.cultivar_name = ''
+        is_expected.to eq false
+      end
+    end
+
+  end
+
   describe 'アソシエーションのテスト' do
     context 'Userモデルとの関係' do
       it 'N:1となっている' do
