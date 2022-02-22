84249ea2f28c986372fb5aad60e6385bc0a5bad6
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-22 14:47:49 +0900

    [Add] DM機能レイアウト追加

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Teraruru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
