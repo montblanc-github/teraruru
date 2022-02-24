de367f2531e274e0789c2c5573352a9b64415d28
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-24 13:55:01 +0900

    [Add] いいねボタン実行前のアクション追加&いいねボタンがすべてに表示されるよう変更

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,10 +1,12 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :user_not_article_user
+
 
   def create
     @article = Article.find(params[:article_id])
     current_user.favorite(@article)
-    # 通知
+   # 通知
     @article.create_notification_by(current_user)
   end
 
@@ -12,4 +14,15 @@ class Public::FavoritesController < ApplicationController
     @article = Article.find(params[:article_id])
     current_user.unfavorite(@article)
   end
+
+  private
+
+  def user_not_article_user
+    article = Article.find(params[:article_id])
+    if article.user == current_user
+      flash[:notice] = "自分の投稿に「いいね」はできません。"
+      redirect_to request.referer
+    end
+  end
+
 end
